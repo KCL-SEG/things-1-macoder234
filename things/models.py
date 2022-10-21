da55271c9f52fbb05859679f5bca4659bed88737
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-10-21 14:16:37 +0100

    Created a user model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    name = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField()
 
 # Create your models here.

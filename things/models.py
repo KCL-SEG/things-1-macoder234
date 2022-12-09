0d4b8b3637515608e1867fdfdfe7c291db721ad6
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-12-09 13:59:33 +0000

    Add model for a thing

--- things/models.py
+++ things/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
@@ -7,4 +8,20 @@ class User(AbstractUser):
     description = models.TextField()
     quantity = models.IntegerField()
 
-# Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False
+    )
+    description = models.CharField(
+        max_length=120,
+        unique=False,
+        blank=True
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[MinValueValidator(0, "Quantity must not be less than 0"),
+                    MaxValueValidator(100, "Quantity must not be more than 100")]
+    )

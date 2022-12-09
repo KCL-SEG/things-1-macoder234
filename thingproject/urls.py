e1acfa39233a5d7c6a11c99d39ea39f67784981c
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-12-09 14:03:18 +0000

    Should work now

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from things.views import home
+from things import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', views.home, name = 'home'),
 ]
--- things/models.py
+++ things/models.py
@@ -1,27 +1,19 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
-
-
-class User(AbstractUser):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
-
+from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(
-        max_length=30,
+        max_length = 30,
         unique=True,
-        blank=False
+        blank = False
     )
     description = models.CharField(
-        max_length=120,
-        unique=False,
-        blank=True
+        max_length = 120,
+        unique = False,
+        blank = True
     )
     quantity = models.IntegerField(
-        unique=False,
-        validators=[MinValueValidator(0, "Quantity must not be less than 0"),
-                    MaxValueValidator(100, "Quantity must not be more than 100")]
+        unique = False,
+        validators = [MinValueValidator(0, "Quantity must not be less than 0"),
+        MaxValueValidator(100, "Quantity must not be more than 100")]
     )
